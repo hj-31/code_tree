f03160321c679ce3ab872f3c9927cc2af7aeafba
hj-31 <chohj9512@kakao.com>
2025-04-09 21:22:59 +0900

    Title: 거울에 레이저 쏘기 2, Time: 46ms, Memory: 16MB, Status: Passed - Codetree

--- 250409/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
+++ 250409/거울에 레이저 쏘기 2/shoot-a-laser-in-the-mirror-2.py
@@ -13,16 +13,12 @@ idx, dir_idx = 3, 0
 for k in range(4*n):
     dx, dy = move_dir[idx]
     x, y = x + dx, y + dy
+    k_dict[k+1] = (x, y, dir_idx)
 
-    a, b = divmod(k, 3)
-    if b == n-1:
-        k_dict[k+1] = (x, y, dir_idx)
+    if k % n == n - 1:
         x, y = x + dx, y + dy
         idx = (idx + 1) % 4
         dir_idx += 1
-    else:
-        k_dict[k+1] = (x, y, dir_idx)
-    # k: (x, y, direction)
 
 def in_range(x, y):
     return 0 <= x < n and 0 <= y < n
@@ -31,21 +27,23 @@ def in_range(x, y):
 # 방향 전환하기
 def change_direction(mirror, dir_idx):
     # / : down <-> left, up <-> right  &  \ : down <-> right, up <-> left
-    to_slash_idx =  [1, 0, 3, 2]
-    to_backslash_idx = [3, 2, 1, 0]
+    to_slash =  [1, 0, 3, 2]
+    to_backslash = [3, 2, 1, 0]
 
-    return to_slash_idx[dir_idx] if mirror == "/" else to_backslash_idx[dir_idx]
+    if mirror == "/":
+        return to_slash[dir_idx]
+    else:
+        return to_backslash[dir_idx]
+
 
 k = int(input())
 
 # 시작 위치
-x, y, idx = k_dict[k]
+start_x, start_y, start_idx = k_dict[k]
 # 격자 안에 시작 위치
-x, y = x + move_dir[idx][0], y + move_dir[idx][1]
+x, y, idx = start_x + move_dir[start_idx][0], start_y + move_dir[start_idx][1], start_idx
+
 cnt = 0
-
-dic = {0: "down", 1: "left", 2: "up", 3: "right"}
-
 # 좌표가 범위 밖이면 끝
 while in_range(x, y):
     # 좌표가 범위 안이니까 거울에 또 튕길 거
