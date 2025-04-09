f974a3387a4e5ece3fc9bfa57eab974c40c1fdc1
hj-31 <chohj9512@kakao.com>
2025-04-09 15:07:11 +0900

    Title: 1이 3개 이상 있는 위치, Time: 85ms, Memory: 19MB, Status: Passed - Codetree

--- 250409/1이 3개 이상 있는 위치/place-more-than-3-ones.py
+++ 250409/1이 3개 이상 있는 위치/place-more-than-3-ones.py
@@ -4,17 +4,19 @@ arr = [list(map(int, input().split())) for _ in range(N)]
 def in_range(x, y):
     return 0 <= x < N and 0 <= y < N
 
-dxy = list(zip([1, -1, 0, 0], [0, 0, 1, -1]))
+def part_cnt(x, y):
+    cnt = 0
+    for dx, dy in zip([1, -1, 0, 0], [0, 0, 1, -1]):
+        nx, ny = x + dx, y + dy
+        if in_range(nx, ny) and arr[nx][ny] == 1:
+            cnt += 1
+    return cnt
+
 
 cnt = 0
 for i in range(N):
     for j in range(N):
-        in_cnt = 0
-        for dx, dy in dxy:
-            nx, ny = i + dx, j + dy
-            if in_range(nx, ny) and arr[nx][ny] == 1:
-                in_cnt += 1
-        if in_cnt >= 3:
+        if part_cnt(i, j) >= 3:
             cnt += 1
 
 print(cnt)
