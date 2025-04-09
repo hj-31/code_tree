d5472d1065b23c2f122cbd534f66d88606bbe63b
hj-31 <chohj9512@kakao.com>
2025-04-09 16:05:12 +0900

    Title: 작은 구슬의 이동, Time: 46ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250409/작은 구슬의 이동/small-marble-movement.py
+++ 250409/작은 구슬의 이동/small-marble-movement.py
@@ -2,19 +2,17 @@ n, t = map(int, input().split())
 r, c, d = input().split()
 r, c = int(r), int(c)
 
-dx = [1, 0, 0, -1]
-dy = [0, -1, 1, 0]
-
-# Up, Down, Right, Left
+# Up, Left, Right, Down
+dxy = list(zip([1, 0, 0, -1], [0, -1, 1, 0]))
+move_dir = {"U": 0, "L": 1, "R": 2, "D": 3}
 
 def in_range(x, y):
     return 1 <= x <= n and 1 <= y <= n
 
-move_dir = { "U": 0, "D": 3, "R": 2, "L": 1}
 direction = move_dir[d]
-
 while t > 0:
-    nx, ny = r + dx[direction], c + dy[direction]
+    dx, dy = dxy[direction]
+    nx, ny = r + dx, c + dy
     
     if not in_range(nx, ny):
         direction = 3 - direction
