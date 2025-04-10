03f7cb5aa2ea01485a96f07a721a23c1b6ca21aa
hj-31 <chohj9512@kakao.com>
2025-04-10 14:35:21 +0900

    Title: 가운데에서 시작하여 빙빙 돌기, Time: 45ms, Memory: 16MB, Status: Wrong Answer - Codetree

--- 250410/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
+++ 250410/가운데에서 시작하여 빙빙 돌기/snail-start-from-center.py
@@ -1,5 +1,48 @@
 n = int(input())
 
+arr = [[0] * n for _ in range(n)]
+cur_x, cur_y = n//2, n//2
+
+# right, up, left, down
+dx, dy = [0, -1, 0, 1], [1, 0, -1, 0]
+move_dir, move_num = 0, 1
+
+def in_range(x, y):
+    return 0 <= x < n and 0 <= y < n
+
+
+# 마지막 직선 이동은 전 방향 진행 수와 같아서 +1 하면 경계 밖 됨
+def end():
+    return in_range(cur_x, cur_y)
+
+
+def move():
+    global cur_x, cur_y
+
+    cur_x += dx[move_dir]
+    cur_y += dy[move_dir]
+
+
+cnt = 1
+while not end():
+    for _ in range(move_num):
+        arr[cur_x][cur_y] = cnt
+        cnt += 1
+
+        move()
+        if end():
+            break
+    
+    move_dir = (move_dir + 1) % 4
+    if move_dir in (0, 2):
+        move_num += 1
+
+for a in arr:
+    print(*a)
+
+
+
+""" 방법2
 def in_range(x, y, small_n, mid):
     return mid - small_n <= x <= mid + small_n and mid - small_n <= y <= mid + small_n
 
@@ -27,3 +70,4 @@ while small_n <= n//2:
 
 for a in arr:
     print(*a)
+"""
