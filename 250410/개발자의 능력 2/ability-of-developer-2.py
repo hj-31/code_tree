009f1d3d2dff4385d0ef17f2fed64c157b9ce924
hj-31 <chohj9512@kakao.com>
2025-04-10 21:35:12 +0900

    Title: 개발자의 능력 2, Time: 76ms, Memory: 18MB, Status: Runtime Error - Codetree

---
+++ 250410/개발자의 능력 2/ability-of-developer-2.py
@@ -0,0 +1,24 @@
+nums = tuple(map(int, input().split()))
+n = len(nums)
+total = sum(nums)
+
+min_diff = 9999999
+
+
+for i in range(n):
+    for j in range(i+1, n): # first team = i, j
+        for k in range(n): # second team = k, l
+            if k in (i, j):
+                continue
+            for l in range(k+1, n):
+                cnt += 1
+                first = nums[i] + nums[j]
+                second = nums[k] + nums[l]
+                third = total - first - second
+
+                biggest = max(first, second, third)
+                smallest = min(first, second, third)
+
+                min_diff = min(min_diff, biggest - smallest)
+
+print(min_diff)
