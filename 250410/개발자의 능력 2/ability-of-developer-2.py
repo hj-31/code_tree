4a12cc30e1ddbf5bad779f0ed56f3ad233e03ecc
hj-31 <chohj9512@kakao.com>
2025-04-10 21:41:44 +0900

    Title: 개발자의 능력 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250410/개발자의 능력 2/ability-of-developer-2.py
+++ 250410/개발자의 능력 2/ability-of-developer-2.py
@@ -4,21 +4,25 @@ total = sum(nums)
 
 min_diff = 9999999
 
+def diff_value(i, j, k, l):
+    first = nums[i] + nums[j]
+    second = nums[k] + nums[l]
+    third = total - first - second
+
+    biggest = max(first, second, third)
+    smallest = min(first, second, third)
+    
+    return biggest - smallest
+
+
+# first team = i, j
 for i in range(n):
-    for j in range(i+1, n): # first team = i, j
-        for k in range(n): # second team = k, l
-            if k in (i, j):
-                continue
-            for l in range(n):
-                if l in (i, j, k):
+    for j in range(i+1, n):
+        # second team = k, l
+        for k in range(n):
+            for l in range(k+1, n):
+                if k in (i, j) or l in (i, j):
                     continue
-                first = nums[i] + nums[j]
-                second = nums[k] + nums[l]
-                third = total - first - second
-
-                biggest = max(first, second, third)
-                smallest = min(first, second, third)
-
-                min_diff = min(min_diff, biggest - smallest)
-
+                min_diff = min(min_diff, diff_value(i, j, k, l))
+                
 print(min_diff)
