3e8c5e0e43e932bc6fa0aae5bb14b54d846630a9
hj-31 <chohj9512@kakao.com>
2025-04-10 16:28:18 +0900

    Title: 두 가지로 열리는 자물쇠, Time: 42ms, Memory: 16MB, Status: Wrong Answer - Codetree

---
+++ 250410/두 가지로 열리는 자물쇠/a-two-way-lock.py
@@ -0,0 +1,25 @@
+n = int(input())
+a1, b1, c1 = map(int, input().split())
+a2, b2, c2 = map(int, input().split())
+
+possible_set = {}
+for i in range(1, 10):
+    lst = [(i+j)%9 if (i+j)%9 != 0 else 9 for j in range(-2, 3)]
+    possible_set[i] = lst
+
+def is_possible(x, y):
+    return x in possible_set[y]
+
+
+def count_possible(a, b, c):
+    cnt = 0
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            for k in range(1, n+1):
+                if is_possible(i, a) and is_possible(j, b) and is_possible(k, c):
+                    cnt += 1
+
+    return cnt
+
+total_cnt = count_possible(a1, b1, c1) + count_possible(a2, b2, c2) - 1
+print(total_cnt)
