2d2908c4234ea105af610ea7466c3816ee7feb11
hj-31 <chohj9512@kakao.com>
2025-04-10 21:37:08 +0900

    Title: 개발자의 능력 2, Time: 52ms, Memory: 16MB, Status: Passed - Codetree

--- 250410/개발자의 능력 2/ability-of-developer-2.py
+++ 250410/개발자의 능력 2/ability-of-developer-2.py
@@ -4,14 +4,14 @@ total = sum(nums)
 
 min_diff = 9999999
 
-
 for i in range(n):
     for j in range(i+1, n): # first team = i, j
         for k in range(n): # second team = k, l
             if k in (i, j):
                 continue
-            for l in range(k+1, n):
-                cnt += 1
+            for l in range(n):
+                if l in (i, j, k):
+                    continue
                 first = nums[i] + nums[j]
                 second = nums[k] + nums[l]
                 third = total - first - second
