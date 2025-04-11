2d50ac8a43eb41ab511d891db83ca622ad62443c
hj-31 <chohj9512@kakao.com>
2025-04-11 14:17:06 +0900

    Title: 겹치지 않는 선분 2, Time: 42ms, Memory: 16MB, Status: Passed - Codetree

--- 250411/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
+++ 250411/겹치지 않는 선분 2/line-segments-that-do-not-overlap-2.py
@@ -1,27 +1,20 @@
 n = int(input())
 arr = [tuple(map(int, input().split())) for _ in range(n)]
-# (x1, 0), (x2, 1) -> (x1, x2)
 
 arr.sort()
 
-max_x1, max_x2 = -1000001, -1000001
-cnt = 0 # 교차 선분
-
-is_crossing = False
-crossing = 1
+max_x2 = -999999999
+not_crossing = []
 
 for x1, x2 in arr:
-    if x2 < max_x2:
-        is_crossing = True
-        crossing += 1
-    else:
-        if is_crossing:
-            is_crossing = False
-            cnt += crossing
-            crossing = 1
+    if x2 > max_x2:
+        not_crossing.append(x2)
         max_x2 = x2
+    else:
+        while not_crossing:
+            before_x2 = not_crossing.pop()
+            if before_x2 < x2:
+                not_crossing.append(before_x2)
+                break
 
-if is_crossing:
-    cnt += crossing
-
-print(n - cnt)
+print(len(not_crossing))
